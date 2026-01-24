952561cd5284ad7eea4d1c4544905f3f65208e5b
Shovan0 <shovannath4039@gmail.com>
2026-01-24 21:10:31 +0530

    added the frontend link in cors

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const app = express();
 // ===== MIDDLEWARES =====
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://departmental-website.onrender.com",
     credentials: true,
   })
 );
